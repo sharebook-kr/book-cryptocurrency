77f059c96c39c8dee34a55efce0d00bf84ef5940
brayden-jo <brayden.jo@outlook.com>
2018-12-09 22:10:14 +0900

    ch06

---
+++ ch06/06_01.py
@@ -0,0 +1,6 @@
+import pybithumb
+
+con_key = "81dd5f25e5daa70b2fff603901d2c09c"
+sec_key = "82333efegeg9eg3e77c573weg34af17a"
+
+bithumb = pybithumb.Bithumb(con_key, sec_key)
---
+++ ch06/06_03.py
@@ -0,0 +1,8 @@
+import pybithumb
+
+con_key = "81dd5f25e5daa70b2fff603901d2c09c"
+sec_key = "82333efegeg9eg3e77c573weg34af17a"
+
+bithumb = pybithumb.Bithumb(con_key, sec_key)
+balance = bithumb.get_balance("BTC")
+print(balance)
